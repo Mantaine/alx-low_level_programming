21cd906708a3ceaae2354f44b787122813a9d3ca
Mantaine <lucytorinke@gmail.com>
2023-06-15 15:51:31 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this function is displaying output to the user
+ * Return: return 0 always a success
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
