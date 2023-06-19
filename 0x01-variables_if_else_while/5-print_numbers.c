8de94876ee1f0f4a366468feb224a37deab05820
Mantaine <lucytorinke@gmail.com>
2023-06-19 11:54:43 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry level
+ * Return: Always a success
+ */
+
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
