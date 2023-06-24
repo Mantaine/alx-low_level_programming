49da5e795d4be3e4194f4516c67a4a5a2df678ce
Mantaine <lucytorinke@gmail.com>
2023-06-23 22:44:39 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print 1 to 100
- * Return: 0
+ * Return: Always 0
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 
 	if ((i % 3 == 0) && (i % 5 == 0))
