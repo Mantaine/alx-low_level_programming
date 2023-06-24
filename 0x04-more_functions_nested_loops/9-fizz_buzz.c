850f2d3d24d707bda442d7b43edb71afebc572cb
Mantaine <lucytorinke@gmail.com>
2023-06-23 22:58:10 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 
-	if ((i % 3 == 0) && (i % 5 == 0))
+	if ((i % 3) == 0 && (i % 5) == 0)
 	{
 		printf("Fizzbuzz");
 	}
@@ -29,11 +29,8 @@ int main(void)
 	{
 		printf("%d", i);
 	}
-	if (i != 100)
-	{
-	printf(" ");
 	}
-	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
