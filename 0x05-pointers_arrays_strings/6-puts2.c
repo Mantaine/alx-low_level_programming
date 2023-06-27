2d91b0ee01e448c8ae186a065b9fc25596d195d0
Mantaine <lucytorinke@gmail.com>
2023-06-27 15:21:26 -0700

    6 puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * iff odd len, n =(lenght of string -1) / 2
+ * @str: input
+ * Return: half of input
+ *
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int h = 0;
+	char *x = str;
+	int o;
+
+	while (*x != '\0')
+
+	{
+	x++;
+	longi++;
+	}
+	h = longi - 1;
+
+	for (o = 0; o <= h ; o++)
+	{
+	if (o % 2 == 0)
+	_putchar(str[0]);
+	}
+}
