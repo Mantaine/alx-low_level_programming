e3ee7751228eadf33d539e4e4e2e08c5d5796ff7
Mantaine <lucytorinke@gmail.com>
2023-06-30 11:09:41 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  *
- * Return void
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
 
