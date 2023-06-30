13800a6496d872e3644bca316b42021d6a63b559
Mantaine <lucytorinke@gmail.com>
2023-06-30 13:14:47 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *string_toupper - changes lowercase to uppercase
+ *@n: input
+ *Return: n
+ */
+char *string_toupper(char *n)
+{
+	int y;
+
+	y = 0;
+	while (n[y] != '\0')
+	{
+		if (n[y] >= 'a' && n[y] <= 'z')
+			n[y] = n[y] - 32;
+		y++;
+	}
+	return (n);
+}
