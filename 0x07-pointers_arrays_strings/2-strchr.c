6b8a7ddc8d03bb58cb078f7f4d6925d639a194a5
Mantaine <lucytorinke@gmail.com>
2023-07-04 11:00:05 -0700

    strch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,9 @@ char *_strchr(char *s, char c)
 	int y;
 
 	for (y = 0; s[y] >= '\0'; y++)
-			{
-			if (s[y] == c)
-			return (s + y);
-			}
-			return (NULL);
+	{
+	if (s[y] == c)
+	return (s + y);
+	}
+	return (NULL);
 }
