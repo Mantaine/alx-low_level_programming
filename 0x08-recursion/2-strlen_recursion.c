d5f2aee74742ce5a26b5ed79b74d626ba386670b
Mantaine <lucytorinke@gmail.com>
2023-07-06 09:18:28 -0700

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: String to count
+ * Return: Integer value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
