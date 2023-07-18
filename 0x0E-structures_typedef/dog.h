69615918af11063e8a9a244dda876e2b4bc65ea0
Mantaine <lucytorinke@gmail.com>
2023-07-18 09:27:54 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,20 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - A new type describin a dog
+ * init_dog - initialize a variable of type stuct dog
+ * @d: pointer to struct do
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: The owner of the dog
  */
-struct dog
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
+	if (d == NULL)
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
-/**
- * dog_t - Typedef of struct do
- */
-typedef struct dog dog_t;
 
-void int_dog(struct dog *d, char *name , float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
